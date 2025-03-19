50bbae02263e7bb307534070902914f7256a0eda
Abhishek kumawat <abhishekkumawat1008@gmail.com>
2025-03-19 22:38:45 +0530

    Made Folder Structure

--- src/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router, Request, Response } from "express";
-
-const router = Router();
-
-router.get("/", (req: Request, res: Response) => {
-    res.json({ message: "Welcome to Express with TypeScript!" });
-});
-
-export default router;
