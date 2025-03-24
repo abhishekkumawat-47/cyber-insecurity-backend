d8fdd692c00691cd1aed9e429e262443bfe8d065
[Utsav Das] <workutsav10@gmail.com>
2025-03-24 19:01:08 +0530

    Transaction and Loan routes

--- src/routes/transactionRoutes.ts
+++ src/routes/transactionRoutes.ts
@@ -101,11 +101,48 @@ router.get("/bySenderAccTransactions/:SenderAcc", async(req: Request, res: Respo
 
 })
 
+router.get("/byUserAcc/:Acc",async(req: Request, res: Response) => {
+  try {
+    const { Acc } = req.params;
+    if (!Acc) {
+      return res.status(400).json({ error: "Sender Account Number is required" });
+    }
+
+    // Fetch transactions where the user is the sender or receiver
+    const transactions = await prisma.transaction.findMany({
+      where: {
+        OR: [{ senderAccNo: Acc }, { receiverAccNo: Acc }],
+      },
+      include:{
+            senderAccount:{
+                include:{
+                    customer:true,
+                }
+            },
+            receiverAccount:{
+                include:{
+                    customer:true,
+                }
+            },
+            loan: true
+        }
+    });
+
+    res.status(200).send(transactions );
+  } catch (error) {
+    console.error("Transaction error:", error);
+    res.status(500).send({ error: "Internal Server Error" });
+  }
+});
+
 
 router.post("/transactions", async (req, res) => {
     try {
         const { senderAccNo, receiverAccNo, amount, transactionType, status, category, description} = req.body;
-
+        
+        if(senderAccNo === receiverAccNo){
+            res.status(400).send({error: "Sender and Receiver Account Number cannot be the same"})
+        }
         const newTransaction = await prisma.transaction.create({
             data: {
                 senderAccNo,
@@ -118,6 +155,34 @@ router.post("/transactions", async (req, res) => {
                 loanId: null
             },
         });
+        
+        if(transactionType === "TRANSFER"){
+            const sender = await prisma.account.findUnique({
+                where:{accNo: senderAccNo}
+            })
+            const receiver = await prisma.account.findUnique({
+                where:{accNo: receiverAccNo}
+            })
+
+            if (!sender || !receiver) {
+                throw new Error('Sender or receiver account not found');
+            }
+
+            const newsender = await prisma.account.update({
+                where:{accNo: senderAccNo},
+                data:{
+                    balance : sender.balance - amount
+                }
+            })
+
+            const newreceiver = await prisma.account.update({
+                where:{accNo: receiverAccNo},
+                data:{
+                    balance : receiver.balance + amount
+                }
+            })
+        }
+        
 
         res.status(201).json(newTransaction);
     } catch (error) {
